fb75462cc2e106a9ef6e5678cdd34d6af699b4c0
seongjun-choi <gtop0913@naver.com>
2025-01-17 13:29:01 +0900

    2798 더 나은 코드로 수정

--- BOJ/2798.py
+++ BOJ/2798.py
@@ -1,17 +1,15 @@
 n, m = map(int, input().split())
 
 array = list(map(int, input().split()))
-array.sort()
 
-sum = []
-length = len(array)
-for i in range(0, length - 2):
-    for j in range(i + 1, length - 1):
-        for k in range(j + 1, length):
-            sum.append(array[i] + array[j] + array[k])
+result = 0
 
-if m in sum:
-    print(m)
-else:
-    new_sum = [x for x in sum if x < m]
-    print(max(new_sum))
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            if array[i] + array[j] + array[k] > m:
+                continue
+            else:
+                result = max(result, array[i] + array[j] + array[k])
+
+print(result)
