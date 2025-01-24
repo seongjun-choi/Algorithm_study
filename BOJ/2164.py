3ffd360836fdd45ca980b57a508afc7ec0213c9d
seongjun-choi <gtop0913@naver.com>
2025-01-24 17:45:39 +0900

    2164 더 깔끔한 코드로 수정

--- BOJ/2164.py
+++ BOJ/2164.py
@@ -6,12 +6,8 @@ n = int(input().rstrip())
 
 queue = deque(range(1, n + 1))
 
-while True:
-    if n == 1:
-        break
+while len(queue) > 1:
     queue.popleft()
     queue.rotate(-1)
-    if len(queue) == 1:
-        break
 
 print(queue[0])
