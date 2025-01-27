9063df29b5de9c2298bab29e2cc0b9be6d956c6c
seongjun-choi <gtop0913@naver.com>
2025-01-27 15:03:46 +0900

    9012 풀이 스택&for-else구문 사용

---
+++ BOJ/9012.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+for _ in range(n):
+    stack = []
+    a = input()
+    for i in a:
+        if i == "(":
+            stack.append(i)
+        elif i == ")":
+            if stack:
+                stack.pop()
+            else:
+                print("NO")
+                break
+    else:
+        if not stack:
+            print("YES")
+        else:
+            print("NO")
