02ff5861a99b1b630dbde48e8f5897d7d0fef3b6
seongjun-choi <gtop0913@naver.com>
2025-07-12 18:48:08 +0900

    스택 수열, counter를 사용하여 스택수열 구현

---
+++ BOJ_cpp/1874.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+vector<char> ops;
+stack<int> st;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int cnt = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        while(cnt <= x){
+            st.push(cnt++);
+            ops.push_back('+');
+        }
+        if(st.top() == x){
+            st.pop();
+            ops.push_back('-');
+        }
+        else{
+            cout << "NO\n";
+            return 0;
+        }
+    }    
+        
+
+    for(char op : ops){
+        cout << op << "\n";
+    }
+
+    return 0;
+}
